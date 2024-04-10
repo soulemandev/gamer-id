422f3175f4e95ce8a7d7aedbd37d2b27356c7867
souleman <joshuaalexander9993@gmail.com>
2024-04-10 11:33:59 -0700

    feat(core): add application functionality

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  AUTH0_DOMAIN: 'dev-wz5rreaa.us.auth0.com',
+  AUTH0_CLIENT_ID: 'JEdJ7OiX4dPFILPKyjDKji1q61aLxBAa',
+  AUTH0_GAMERID_CLAIM: 'https://gamer_id',
+  LAMBDA_API_URL: '/.netlify/functions'
+};
