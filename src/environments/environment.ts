a704bab5ba266550f71d10f7cd0e897db774cce1
souleman <joshuaalexander9993@gmail.com>
2024-04-20 17:20:00 -0700

    feat(core): add application functionality

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  AUTH0_DOMAIN: 'dev-wz5rreaa.us.auth0.com',
+  AUTH0_CLIENT_ID: 'JEdJ7OiX4dPFILPKyjDKji1q61aLxBAa',
+  AUTH0_GAMERID_CLAIM: 'https://gamer_id',
+  LAMBDA_API_URL: '/.netlify/functions'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
