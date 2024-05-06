fafa859e7ef0156e828224451463bbe22fd3deb4
souleman <joshuaalexander9993@gmail.com>
2024-05-06 12:27:47 -0700

    feat(ui): build user interface

---
+++ src/app/components/modules/persona/Blizzard/test-blizzard1/test-blizzard1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'test-blizzard1',
+  templateUrl: './test-blizzard1.component.html',
+  styleUrls: ['./test-blizzard1.component.css']
+})
+export class TestBlizzard1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
