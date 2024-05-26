35de7440968556c7154a4d7cc78946cd4e2eaadb
souleman <joshuaalexander9993@gmail.com>
2024-05-25 17:33:07 -0700

    feat(ui): build user interface

---
+++ src/app/components/modules/persona/Steam/test-steam1/test-steam1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'test-steam1',
+  templateUrl: './test-steam1.component.html',
+  styleUrls: ['./test-steam1.component.css']
+})
+export class TestSteam1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
